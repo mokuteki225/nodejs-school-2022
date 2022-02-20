e451fab56d4632d419e0f811877f889f5f0ca88d
mokuteki225 <kondratiuk011@gmail.com>
2022-02-20 20:51:03 +0200

    feat: task0

---
+++ 03_lecture/homework/task0.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import puppeteer from 'puppeteer/lib/cjs/puppeteer/node-puppeteer-core';
+
+const getPageContent = async (url: string) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setDefaultNavigationTimeout(0);
+  await page.goto(url);
+
+  const content = await page.content();
+
+  browser.close();
+
+  return content;
+}
+
+const main = async () => {
+  const path = process.argv[2];
+
+  if (!fs.existsSync(path)) {
+    throw new Error('Given path does not exist');
+  }
+
+  const fileName = path.split('/').pop().split('.')[0];
+  const folderPath = fileName + '_pages';
+
+  const data = await fs.promises.readFile(path);
+  const parsedData = JSON.parse(data.toString());
+
+  await fs.promises.mkdir(folderPath);
+
+  for (let i = 0; i < parsedData.length; i++) {
+    const content = await getPageContent(parsedData[i]);
+
+    await fs.promises.writeFile(folderPath + `/${i}.txt`, content);
+  }
+}
+
+main();
